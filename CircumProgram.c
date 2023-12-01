2e7e5cf62db1abfe689482a5a5f2951970547d51
Fahad <fahad.alsalihi05@gmail.com>
2023-11-30 20:35:18 -0800

    finally finished

--- CircumProgram.c
+++ CircumProgram.c
@@ -5,13 +5,18 @@ int main(){
 const double PI = 3.14159;
 double radius;
 double circumference;
+double area;
+
+
+
+
 printf("\nenter radius of circle");
 scanf("%lf", &radius);
 
 circumference =  2 * PI * radius;
-    
-printf("circumference: %lf", circumference);
-
+area = PI * radius * radius;   
+printf("\ncircumference: %lf", circumference);
+printf("\narea is %lf", area);
     return 0;
 }
 //// another test to see how to update
